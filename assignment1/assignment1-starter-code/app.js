fc033d2d15d66af65e4233d2dd5bb83460c9eb57
Xiaohu Zhu <xhzhu.nju@gmail.com>
2020-04-06 00:03:50 +0800

    bonus

--- assignment1/assignment1-starter-code/app.js
+++ assignment1/assignment1-starter-code/app.js
@@ -8,19 +8,29 @@ LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
     $scope.inputs = "";
     $scope.message = "";
+    $scope.colorFont = "green";
 
     $scope.lunchCheck = function () {
         if ($scope.inputs == "") {
             $scope.message = "Please enter data first!";
+            $scope.colorFont = "red";
+            document.getElementById("lunch-menu").style.borderColor = "red";
             return;
         }
         var totalNum = 0;
-        totalNum = $scope.inputs.split(',').length;
+        totalItems = $scope.inputs.split(',');
+        for (var i = 0; i < totalItems.length; i++) {
+            if (totalItems[i] != "") {
+                totalNum += 1;
+            }
+        }
+
         if (totalNum < 4 ) {
             $scope.message = "Enjoy!";
         } else {
             $scope.message = "Too much!";
         }
+        document.getElementById("lunch-menu").style.borderColor = "green";
     };
 
     $scope.getMessage = function() {
